eb2f4db3c0c9fdb56b8a77ab618c6329022e2dbf
swaminaing <smnaing9.smn@gmail.com>
2025-03-06 22:11:58 +0630

    create a foundation (basic UI components)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import Header from "./components/Header.tsx";
+import Shop from "./components/Shop.tsx";
+import Product from "./components/Product.tsx";
+import { DUMMY_PRODUCTS } from "./dummy-products.ts";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+      <Shop>
+        {DUMMY_PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Product {...product} />
+          </li>
+        ))}
+      </Shop>
+    </>
+  );
 }
 
 export default App;
