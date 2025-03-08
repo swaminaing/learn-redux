1a217b0505ea27c80587eb9669c787fb330722e8
swaminaing <smnaing9.smn@gmail.com>
2025-03-08 22:02:21 +0630

    manipulate removing function in redux cart slice

--- src/store/cart-slice.ts
+++ src/store/cart-slice.ts
@@ -37,6 +37,16 @@ export const cartSlice = createSlice({
         state.items.push({ ...action.payload, quantity: 1 });
       }
     },
-    removeFromCart() {},
+    removeFromCart(state, action: PayloadAction<string>) {
+      const itemIndex = state.items.findIndex(
+        (item) => item.id === action.payload
+      );
+
+      if (state.items[itemIndex].quantity === 1) {
+        state.items.splice(itemIndex, 1);
+      } else {
+        state.items[itemIndex].quantity--;
+      }
+    },
   },
 });
